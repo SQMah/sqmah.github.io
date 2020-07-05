dd2791476c797adfd1c0be5590c8ed5c537c55a9
Shao-Qian Mah <sqmah@berkeley.edu>
2020-07-05 00:23:04 -0700

    Updates

---
+++ precache-manifest.13112462fe07cbab186a8f4979487e13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5038d274303827be408626c03e65129",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a55dc55026a97cb4898d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "755214dcd6d8ad0ae452",
+    "url": "/static/js/2.27129fac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27129fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55dc55026a97cb4898d",
+    "url": "/static/js/main.bcc2f342.chunk.js"
+  },
+  {
+    "revision": "344fd4cf40b6d51e733d",
+    "url": "/static/js/runtime-main.4487ed6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
